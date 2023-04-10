ee93bfe8cddaedbdee3ae92596eda6a5cc526f61
jakariamasum <jmphy698@gmail.com>
2023-04-11 01:27:16 +0600

    job category added

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-  import { faCoffee } from '@fortawesome/free-solid-svg-icons'
-  const element = <FontAwesomeIcon icon="fa-solid fa-calculator" />
+import ReactDOM from 'react-dom'
+
   
 
 
 const Category = (props) => {
     // console.log(props.category)
-    const {name,jobs_available}=props.category;
+    const {name,jobs_available,logo}=props.category;
     return (
-        <div className='w-[311px] h-[243px] bg-[#F9F9FF] flex'>
+        <div className='w-[311px] h-[243px] bg-white pl-14'>
+            <img src={logo} className="w-[40px] h-[40px] mt-10 mb-7" />
             <div className='px-2 w-[311px] h-[243px]'>
-                <h1>{name}</h1>
-                <p>{jobs_available}</p>
+                <h1 className='text-[#474747] font-bold text-xl'>{name}</h1>
+                <p className='text-[#A3A3A3] font-semibold mt-2'>{jobs_available} Jobs Available</p>
             </div>
             
         </div>
--- src/components/FeaturedJobs/JobCard/JobCard.jsx
+++ src/components/FeaturedJobs/JobCard/JobCard.jsx
@@ -8,13 +8,17 @@ const JobCard = (props) => {
                 <img src={logo} alt="" />
                 <div className="card-body">
                     <h2 className="card-title">{job_title}</h2>
-                    <p>{company_contact.address}</p>
+                    <p className='text-[#757575] font-semibold text-xl mt-2 pb-4'>{company_contact.name}</p>
                     <div className="card-actions">
                         <button className="btn btn-outline btn-accent">{job_type[0]}</button>
                         <button className="btn btn-outline btn-accent">{job_type[1]}</button>
-
+                    </div>
+                    <div className='flex text-[#757575]'>
+                        <p>{company_contact.address}</p>
+                        <p>{salary}</p>
                     </div>
                 </div>
+                    <button className='btn btn-accent'>View Details</button>
             </div>
 
         </div>
