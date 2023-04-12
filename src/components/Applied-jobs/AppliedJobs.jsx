a761b0d1b56985d1e2f09d85f4f2cfb3ed190d11
jakariamasum <jmphy698@gmail.com>
2023-04-12 20:41:58 +0600

    filter method added

--- src/components/Applied-jobs/AppliedJobs.jsx
+++ src/components/Applied-jobs/AppliedJobs.jsx
@@ -7,6 +7,7 @@ const AppliedJobs = () => {
     const saved = [];
     const [job, setJob] = useState(getShoppingCart());
     const [allJob, setAllJob] = useState([]);
+    const [selectedButton, setSelectedButton] = useState('all');
 
     useEffect(() => {
         fetch('/featuredJobsData.json')
@@ -22,16 +23,36 @@ const AppliedJobs = () => {
         jobInDb.push(newApplied)
     }
 
+    const handleButtonClick = (buttonType) => {
+        setSelectedButton(buttonType);
+    }
+
+    let filteredJobs = jobInDb;
+    if (selectedButton === 'remote') {
+        filteredJobs = jobInDb.filter(job => job.job_type.includes('Remote'));
+    } else if (selectedButton === 'onsite') {
+        filteredJobs = jobInDb.filter(job => job.job_type.includes('Onsite'));
+    }
+
     return (
         <div>
-            {
-                jobInDb.map(job => {
-                    if (job) {
-                        return (
-                            <AppliedJobCard key={job.id} job={job}></AppliedJobCard>
-                        )
-                    }
-                })
+            <div className='flex gap-8 justify-center mt-8'>
+                <button className={`btn ${selectedButton === 'all' ? 'btn-primary' : 'btn-outline'} text-[#A4A3FF] border border-[#A4A3FF]`} onClick={() => handleButtonClick('all')}>All</button>
+                <button className={`btn ${selectedButton === 'remote' ? 'btn-primary' : 'btn-outline'} text-[#A4A3FF] border border-[#A4A3FF]`} onClick={() => handleButtonClick('remote')}>Remote</button>
+                <button className={`btn ${selectedButton === 'onsite' ? 'btn-primary' : 'btn-outline'} text-[#A4A3FF] border border-[#A4A3FF]`} onClick={() => handleButtonClick('onsite')}>Onsite</button>
+            </div>
+{
+                filteredJobs.length === 0 ? (
+                    <p className="text-center mt-8">No jobs found.</p>
+                ) : (
+                    filteredJobs.map(job => {
+                        if (job) {
+                            return (
+                                <AppliedJobCard key={job.id} job={job}></AppliedJobCard>
+                            )
+                        }
+                    })
+                )
             }
         </div>
     );
